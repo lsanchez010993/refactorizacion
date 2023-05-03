06421c00750904fce3fba2938883452016bcba82
Luis <lsanchez4rsapalomera.cat>
2023-05-03 14:00:33 +0200

    actualizado

--- practica_REFAC/src/Main.java
+++ practica_REFAC/src/Main.java
@@ -3,37 +3,65 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String p = "Proves";
+    static Scanner scan = new Scanner(System.in);
 
+    //REFACT1: METODO: menu()
+    public static int menu() {
+        System.out.println("1. ");
+        System.out.println("2. ");
+        System.out.println("3. ");
+        System.out.println("4. ");
+        System.out.println("5. ");
+        System.out.println("0. Acabar");
+
+        int opcio = scan.nextInt();
+        scan.nextLine();
+        return opcio;
+    }
+    //REFACT1
+    public static void opcio1() {
+        System.out.println("intro: ");
+        int num1 = scan.nextInt();
+        System.out.println("intro: ");
+        int num2 = scan.nextInt();
+        if (max(num1, num2)) {
+            System.out.println("aaa");
+        } else System.out.println("bbb");
+    }
+
+    public static void opcio2() {
+        System.out.println("intro: ");
+        int num1 = scan.nextInt();
+        System.out.println("intro: ");
+        int num2 = scan.nextInt();
+        if (max(num1, num2)) {
+            System.out.println("aaa");
+        } else System.out.println("bbb");
+    }
+
+    public static void main(String[] args) {
         int opcio;
         do {
-            System.out.println("1. ");
-            System.out.println("2. ");
-            System.out.println("3. ");
-            System.out.println("4. ");
-            System.out.println("5. ");
-            System.out.println("0. Acabar");
+            opcio = menu();
 
-            opcio = scan.nextInt();
-            scan.nextLine();
+            String p = "Proves";
+
+
             switch (opcio) {
                 case 1:
-                    System.out.println("intro: ");
-                    int num1 = scan.nextInt();
-                    System.out.println("intro: ");
-                    int num2 = scan.nextInt();
-                    if (max(num1,num2)) {
-                        System.out.println("aaa");
-                    }
-                    else System.out.println("bbb");
+                    //Refact2: opcio1()
+//                    System.out.println("intro: ");
+//                    int num1 = scan.nextInt();
+//                    System.out.println("intro: ");
+//                    int num2 = scan.nextInt();
+//                    if (max(num1, num2)) {
+//                        System.out.println("aaa");
+//                    } else System.out.println("bbb");
+                    opcio1();
                     break;
                 case 2:
-                    double a=2;
-                    double b=3;
-                    double c=1;
-                    calcEquacioSegongrau(a, b, c);
+                    //REFACT2
+                    calcEquacioSegongrau(2, 3, 1);
                     break;
                 case 3:
                     List<OrderLineItem> lineItems = null;
@@ -46,8 +74,9 @@ public class Main {
             }
         } while (opcio != 0);
     }
+
     public static boolean max(int a, int b) {
-        if(a > b) {
+        if (a > b) {
             return true;
         } else if (a == b) {
             return false;
@@ -55,6 +84,7 @@ public class Main {
             return false;
         }
     }
+
     public static void calcEquacioSegongrau(double a, double b, double c) {
         double D = b * b - 4 * a * c;
         if (D > 0) {
@@ -62,16 +92,15 @@ public class Main {
             x1 = (-b - Math.sqrt(D)) / (2 * a);
             x2 = (-b + Math.sqrt(D)) / (2 * a);
             System.out.println("x1 = " + x1 + ", x2 = " + x2);
-        }
-        else if (D == 0) {
+        } else if (D == 0) {
             double x;
             x = -b / (2 * a);
             System.out.println("x = " + x);
-        }
-        else {
+        } else {
             System.out.println("Equation has no roots");
         }
     }
+
     public static class Human {
         private String name;
         private String age;
@@ -80,6 +109,7 @@ public class Main {
         private String street;
         private String house;
         private String quarter;
+
         public String obtenirAdrecaCompleta() {
             StringBuilder result = new StringBuilder();
             return result
@@ -113,21 +143,22 @@ public class Main {
             return subtotal + tax;
         }
     }
-
-     public class OrderLineItem {
-        private String productName;
-        private int quantity;
-        private double price;
-
-        public OrderLineItem(String productName, int quantity, double price) {
-            this.productName = productName;
-            this.quantity = quantity;
-            this.price = price;
-        }
-        public double getPrice() {
-            return price * quantity;
-        }
-    }
+//REFACT4: eXTRACCIO DE CLASE: OrderLineItem
+//    public class OrderLineItem {
+//        private String productName;
+//        private int quantity;
+//        private double price;
+//
+//        public OrderLineItem(String productName, int quantity, double price) {
+//            this.productName = productName;
+//            this.quantity = quantity;
+//            this.price = price;
+//        }
+//
+//        public double getPrice() {
+//            return price * quantity;
+//        }
+//    }
 
     public class Customer {
         private String firstName;
